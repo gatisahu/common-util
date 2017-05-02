604669d4fb8532390b44389fc8b979b179659783
Gatikrushna Sahu <gatikrushna.sahu@in.pega.com>
2017-05-02 23:12:42 +0530

    commit

---
+++ src/main/java/com/gati/common/collection/Stack.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.gati.common.collection;
+
+/**
+ * @author sahug
+ *
+ */
+public interface Stack {
+
+}
---
+++ src/test/java/com/gati/common/collection/TestStack.java
@@ -0,0 +1,14 @@
+package com.gati.common.collection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStack {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
